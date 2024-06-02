0e9c0dea04a5cc5d34cbf61b651d4b3f669d3c34
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-06-02 20:39:53 -0300

    return domain status

--- 2-registro.py
+++ 2-registro.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # 1 - Acessando o site
+
 browser = webdriver.Firefox()
 browser.get("https://registro.br")
 
@@ -14,3 +15,10 @@ elem.send_keys("botscompython.com.br")
 elem.send_keys(Keys.ENTER)
 time.sleep(5)
 browser.save_screenshot("dominio.png")
+
+# 3 - Buscando informações
+results = browser.find_elements(By.TAG_NAME, "strong")
+# import pdb # debugger
+# pdb.set_trace() # store all matching elements for internal use and debugging
+print(f"Domínio {results[1].text} está {results[2].text}")
+# browser.quit()
