b6a311e30a90ad969ce82893bf9719124a2cc179
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-06-02 20:25:07 -0300

    locate items with the search bar

--- 1-acessando-page.py
+++ 1-acessando-page.py
@@ -1,7 +1,16 @@
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # 1 - Utilizando o WebDriver
 browser = webdriver.Firefox()
 
 browser.get('http://www.amazon.com.br')
+
+# 2 - Acessando elementos numa página
+elem = browser.find_element(By.ID, 'twotabsearchtextbox')
+elem.send_keys('ps5')
+elem.send_keys(Keys.ENTER)
+
 browser.quit()
